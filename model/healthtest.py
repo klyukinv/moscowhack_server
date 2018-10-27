8c40d7eca1093d2c23e2837cfc3cc73e22a94c4f
klyukin <vdklyukin@edu.hse.ru>
2018-10-27 19:57:51 +0300

    bug fixed. old version

--- model/healthtest.py
+++ model/healthtest.py
@@ -1,24 +1,29 @@
 import numpy as np
 import pandas as pd
 
-sym_dis = np.genfromtxt('symptom_disease.csv', delimiter=',')
-sym_title = pd.read_csv('sym_t.csv')
-dis_title = pd.read_csv('dia_t.csv', index_col='did')
-max_weights = np.load("max_weights.npy")
+sym_dis = np.genfromtxt('model/symptom_disease.csv', delimiter=',')
+sym_title = pd.read_csv('model/sym_t.csv', index_col='syd')
+dis_title = pd.read_csv('model/dia_t.csv', index_col='did')
+max_weights = np.load("model/max_weights.npy")
 
 max_valuable = 262  # chest pain. counted "by hands"
 max_leaf = 18
 
 
 class HealthTest:
-    def __init__(self):
+    def __init__(self, test_id: str = None):
+        if test_id is None:
+            raise Exception("test id can not be None")
+        self.id = test_id
         self.a = np.zeros(sym_dis.shape[0])
         self.asked = np.zeros(sym_dis.shape[0], dtype=bool)
         self.diagnosis = None
         self.last_asked = None
         self.last_positive = None
         self.last_positive_dist = 0
-        self.scalars_sym = np.load("scalars_sym.npy")
+        self.scalars_sym = np.load("model/scalars_sym.npy")
+        self.diagnosis = None
+        self.last_symptom = None
 
     def top_disease_cos(self):
         best_dis = -1
@@ -36,7 +41,7 @@ class HealthTest:
     def most_suitable(self) -> (bool, int):
         sym_i = -1
         best_disease, bd_index = self.top_disease_cos()
-        if best_disease >= 0.15 or np.count_nonzero(self.asked) > 6:
+        if best_disease >= 0.15 or np.count_nonzero(self.asked) > 6 and self.last_positive is not None:
             return True, bd_index
         if self.a[self.last_asked] > 0:
             row = self.scalars_sym[:, self.last_asked + 1]
@@ -49,12 +54,13 @@ class HealthTest:
                 return False, self.scalars_sym[self.last_positive, :].argsort()[-(self.last_positive_dist + 1):][::-1][
                     self.last_positive_dist - 1]
             else:
-                nz = np.count_nonzero(self.asked)
-                return False, max_weights.argsort()[-(nz + 1):][::-1][nz - 1]
+                return False, max_weights.argsort()[-(np.count_nonzero(self.asked) + 1):][::-1][
+                    np.count_nonzero(self.asked) - 1]
 
     def get_question(self) -> dict:
         try:
             if not np.any(self.asked):
+                self.last_symptom = max_valuable
                 return {"symptom": max_valuable}
             else:
                 result = self.most_suitable()
@@ -62,6 +68,7 @@ class HealthTest:
                     self.diagnosis = result[1]
                     return {"diagnosis": self.diagnosis}
                 else:
+                    self.last_symptom = result[1]
                     return {"symptom": result[1]}
         except Exception as e:
             return {"error": str(e)}
@@ -69,8 +76,12 @@ class HealthTest:
     def scale(self, symptom: int, value: int) -> int:
         return value * max_weights[symptom - 1] / 5
 
-    def get_response(self, symptom: int, value: int) -> dict:
+    def get_response(self, value: int) -> dict:
         try:
+            if self.last_symptom is None:
+                raise Exception()
+            symptom = self.last_symptom
+
             if self.diagnosis is not None:
                 return {"diagnosis": dis_title.loc[self.diagnosis]['diagnose'],
                         "doctor": dis_title.loc[self.diagnosis]['doctor']}
@@ -93,4 +104,14 @@ class HealthTest:
 
 
 def int_to_sym(sym_index: int):
-    return sym_title.loc[sym_index]
+    try:
+        return str(sym_title.loc[sym_index][0]).replace('\u000b', '/')
+    except:
+        return "NaN"
+
+
+def int_to_dis(dis_index: int):
+    try:
+        return str(dis_title.loc[dis_index][0]).replace('\u000b', '/')
+    except:
+        return "NaN"
